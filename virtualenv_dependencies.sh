2144b558b3a3b50725cb29f0826cf8a8afb93c51
Joe Cooter <jcooter@oceanius.com>
2015-10-05 23:48:58 -0400

    adding packages to virtualenv_dependencies.sh

--- virtualenv_dependencies.sh
+++ virtualenv_dependencies.sh
@@ -2,4 +2,4 @@
 
 yum install -y gcc libxml2-devel postgresql-devel libyaml-devel  \
     python3-pillow-devel python-pillow-devel postgresql-plpython \
-    postgresql-plpython3 python3-wheel python-wheel
+    postgresql-plpython3 python3-wheel python-wheel libffi-devel libffi

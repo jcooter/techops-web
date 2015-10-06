25a1b123e8a79565b4bc5127cc68e557d29d2a0b
Joe Cooter <jcooter@oceanius.com>
2015-10-05 23:40:21 -0400

    adding virtualenv_requirements.sh

---
+++ virtualenv_requirements.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+yum install -y gcc libxml2-devel postgresql-devel libyaml-devel  \
+    python3-pillow-devel python-pillow-devel postgresql-plpython \
+    postgresql-plpython3 python3-wheel python-wheel

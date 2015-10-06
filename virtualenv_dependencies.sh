83ae40650398a1ae81ed32a84bc1383eb8371ac8
Joe Cooter <jcooter@oceanius.com>
2015-10-06 01:34:29 -0400

    adding openssl to virtualenv_dependencies.sh

--- virtualenv_dependencies.sh
+++ virtualenv_dependencies.sh
@@ -2,4 +2,5 @@
 
 yum install -y gcc libxml2-devel postgresql-devel libyaml-devel  \
     python3-pillow-devel python-pillow-devel postgresql-plpython \
-    postgresql-plpython3 python3-wheel python-wheel libffi-devel libffi
+    postgresql-plpython3 python3-wheel python-wheel libffi-devel \
+    libffi openssl openssl-devel
